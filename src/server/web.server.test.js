21a2ada34a607f9295b8ec62dfefe0aa80ea3d01
gbarrbrown <gbb7899@gmail.com>
2019-03-09 15:39:32 +1300

    fix typos in web.server.test.js

--- src/server/web.server.test.js
+++ src/server/web.server.test.js
@@ -1,5 +1,4 @@
 import WebServer from './web.server'
-import { testNameToKey } from 'jest-snapshot/build/utils';
 
 describe('Started', () => {
     let webServer = null
@@ -8,12 +7,12 @@ describe('Started', () => {
         webServer = new WebServer()
     })
 
-    testNameToKey('should start and trigger a callback', async () => {
+    test('should start and trigger a callback', async () => {
         let promise = webServer.start()
-        await expectExport(promise).resolves.toBeUndefined()
+        await expect(promise).resolves.toBeUndefined()
     })
 
-    testNameToKey('should stop and trigger a callback', async () => {
+    test('should stop and trigger a callback', async () => {
         let promise = webServer.stop()
         await expect(promise).resolves.toBeUndefined()
     })
